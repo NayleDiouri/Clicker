170ac367d1345172e8b1ce91d0e53bc09d27b026
NayleDiouri <93705247+NayleDiouri@users.noreply.github.com>
2024-01-10 15:32:54 +0000

    début

--- Assets/Scripts/Berries.cs
+++ Assets/Scripts/Berries.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Berries", menuName ="berry", order = 100)]
 public class Berries : ScriptableObject
 {
+
+    public string berryName;
+    public Sprite berrySprite;
+    public int berryHP;
+
     // Start is called before the first frame update
     void Start()
     {
