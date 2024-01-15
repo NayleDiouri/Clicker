2a04acbd04a385170c1030f02971f17f5f5940ca
NayleDiouri <93705247+NayleDiouri@users.noreply.github.com>
2024-01-15 14:42:15 +0100

    ajout boutons

---
+++ Assets/Scripts/CurrentBenty.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentBenty : MonoBehaviour
+{
+    public SpriteRenderer sR;
+    public Berries[] berrieToRead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
