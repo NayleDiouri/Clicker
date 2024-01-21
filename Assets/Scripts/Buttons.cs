5ac3476d25e9ed5443d3af173d4dab6b1a62d08f
JujuLaBest35 <93705247+NayleDiouri@users.noreply.github.com>
2024-01-21 19:55:02 +0100

    Final

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class Buttons : MonoBehaviour
 {
     public CurrentBerry cB;
     public Berries[] berry;
-    public float digPrice, gainPrice;
+    public float digPrice, gainPrice, autoClickPrice;
+    public TextMeshProUGUI blueText, redText, yellowText;
 
 
 
@@ -17,6 +21,9 @@ public class Buttons : MonoBehaviour
         berry[2].money = 10;
         berry[3].money = 20;
         berry[4].money = 50;
+        blueText.text = "better digging strenght " + digPrice;
+        redText.text = "Better auto click " + autoClickPrice;
+        yellowText.text = "More money! " + gainPrice;
     }
     public void UpgradeDig()
     {
@@ -26,9 +33,24 @@ public class Buttons : MonoBehaviour
             cB.moneyText.text = "Money : " + cB.totalMoney;
             digPrice += 10;
             cB.digStrenght += 1;
+            blueText.text = "" + digPrice;
+
         }
     }
 
+    public void UpgradeAutoClick()
+    {
+        if (cB.totalMoney >= autoClickPrice)
+        {
+            cB.autoClick += 1;
+            cB.autoClickSpeed *= 0.92f;
+            cB.totalMoney -= autoClickPrice;
+            autoClickPrice *= 1.4f;
+            cB.moneyText.text = "Money : " + cB.totalMoney;
+            redText.text = "" + autoClickPrice;
+
+        }
+    }
     public void UpgradeGain()
     {
         if(cB.totalMoney >= gainPrice)
@@ -41,6 +63,7 @@ public class Buttons : MonoBehaviour
             berry[2].money *= 1.4f;
             berry[3].money *= 1.4f;
             berry[4].money *= 1.4f;
+            yellowText.text = "" + gainPrice;
         }
     }
 
--- Assets/Scripts/CurrentBerry.cs
+++ Assets/Scripts/CurrentBerry.cs
@@ -9,16 +9,15 @@ public class CurrentBerry : MonoBehaviour
     public SpriteRenderer sR;
     public Berries[] berrieToRead;
     public Berries currentBerry;
-    public float digStrenght; 
-    public float currentBerryHP;
+    public float digStrenght, currentBerryHP, totalMoney, upgradeHp, autoClick, autoClickSpeed;
     public BerryBar berryBar;
     public TextMeshProUGUI moneyText;
-    public float totalMoney;
 
     void Start()
     {
         GenerateBerry();
         berryBar.SetMaxHealth(currentBerryHP);
+        StartCoroutine(AutoClic());
     }
 
     private void OnMouseDown()
@@ -32,12 +31,27 @@ public class CurrentBerry : MonoBehaviour
         }
 
     }
-
+    public IEnumerator AutoClic()
+    {
+        while (true)
+        {
+            currentBerryHP -= autoClick;
+            yield return new WaitForSeconds(autoClickSpeed);
+            berryBar.SetHealth(currentBerryHP);
+            if (currentBerryHP <= 0)
+            {
+                totalMoney += currentBerry.money;
+                moneyText.text = "Money : " + totalMoney;
+                GenerateBerry();
+            }
+        }
+    }
     public void GenerateBerry()
     {
+        upgradeHp += 0.1f;
         currentBerry = berrieToRead[Random.Range(0, berrieToRead.Length)];
         sR.sprite = currentBerry.berrySprite;
-        currentBerryHP = currentBerry.berryHP;
+        currentBerryHP = currentBerry.berryHP * upgradeHp;
         berryBar.SetMaxHealth(currentBerryHP);
     }
     public void DigBerry()
